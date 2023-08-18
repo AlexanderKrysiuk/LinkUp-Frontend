362367c77f7559f52425037e15a584608346c098
marcioszka <monkiewicz.marta@gmail.com>
2023-08-18 15:16:58 +0200

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-// import BadgeComponent from '@components/navbar/badge/Badge.tsx';
-// import ButtonComponent from '@components/navbar/button/Button.tsx';
-import RegistrationForm from '@components/registrationForm/RegistrationForm.tsx';
 import useScript from '@hooks/useScript.tsx';
+import RegistrationForm from './components/registrationForm/RegistrationForm.tsx';
 
 import React from 'react';
 
-import './App.css';
+import './components/registrationForm/RegistrationForm.css';
 
-//import imageIcon from './assets/image-regular.svg';
+import './App.css';
 
 function App() {
 	useScript('../scripts/themeSwitcher.ts');
 	return (
 		<>
-			{/* <BadgeComponent
-				text='sample'
-				source={imageIcon}
-			/>
-			<ButtonComponent /> */}
 			<RegistrationForm />
 		</>
 	);
