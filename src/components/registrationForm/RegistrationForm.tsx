e1e319fa8b88260672ddf83862e02bee492acd13
marcioszka <monkiewicz.marta@gmail.com>
2023-08-18 15:16:30 +0200

    Implement zod library | add user type input

--- src/components/registrationForm/RegistrationForm.tsx
+++ src/components/registrationForm/RegistrationForm.tsx
@@ -1,123 +1,160 @@
-import React, { useEffect, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import { ZodType, z } from 'zod';
 import './RegistrationForm.css';
 
 export default function RegistrationForm() {
-	const {
-		register,
-		getValues,
-		handleSubmit,
-		formState: { errors, isValid },
-		setError,
-		clearErrors,
-	} = useForm({
-		defaultValues: {
-			login: '',
-			email: '',
-			password: '',
-			confirmedPassword: '',
-		},
-		mode: 'onChange',
-	});
-
-	const [isFormValid, setFormValid] = useState(false);
-	const [isLoginValid, setLoginValid] = useState(false);
-	const [isEmailValid, setEmailValid] = useState(false);
-	const [isPasswordValid, setPasswordValid] = useState(false);
-	const [isConfirmedPasswordValid, setConfirmedPasswordValid] =
-		useState(false);
-
-	useEffect(() => {
-		setFormValid(isValid);
-	}, [isValid]);
-
-	const [serverError, setServerError] = useState<string | null>();
-
-	const submitForm = async (data: any) => {
-		try {
-			const response = await fetch(
-				'https://localhost:7238/api/users/signup',
-				{
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify(data),
-				},
-			)
-				.then((res) => {
-					if (res.ok) {
-						alert("You're registered!");
-						setServerError(null);
-					} else {
-						return res.json();
-					}
-				})
-				.then((data) => {
-					switch (data.code) {
-						case 400:
-							setServerError(
-								`${data.message} [${data.code} Bad request]`,
-							);
-							break;
-						case 401:
-							setServerError(
-								`${data.message} [${data.code} Unauthorized]`,
-							);
-							break;
-						case 409:
-							setServerError(
-								`${data.message} [${data.code} Conflict]`,
-							);
-							break;
-					}
-				});
-		} catch (err) {
-			//TODO: check error after registration: TypeError
-			//setServerError(`${err}`);
-		}
+	type FormData = {
+		userType: string;
+		firstName: string;
+		lastName: string;
+		email: string;
+		password: string;
+		confirmedPassword: string;
 	};
 
-	const validatePasswordConfirmation = () => {
-		const confirmedPasswordValue = getValues('confirmedPassword');
-		const passwordValue = getValues('password');
+	const user: ZodType<FormData> = z
+		.object({
+			userType: z.string(),
+			firstName: z
+				.string()
+				.min(2, { message: 'Must be at least 2 characters long' }),
+			lastName: z
+				.string()
+				.min(2, { message: 'Must be at least 2 characters long' }),
+			email: z
+				.string()
+				.email()
+				.regex(
+					/^[a-zA-Z0-9]+([.\-_]?[a-zA-Z0-9])*@[a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.[a-zA-Z]{2,}$/,
+					{ message: 'Invalid email address' },
+				),
+			password: z
+				.string()
+				.regex(/^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).{8,}$/, {
+					message:
+						'Password must be at least 8 characters long and contain at least one uppercase letter, one special character, and one digit.',
+				}),
+			confirmedPassword: z.string(),
+		})
+		.refine((data) => data.password === data.confirmedPassword, {
+			message: 'Passwords do not match.',
+			path: ['confirmedPassword'],
+		});
 
-		if (confirmedPasswordValue !== passwordValue) {
-			setError('confirmedPassword', { message: 'Passwords must match.' });
-			setConfirmedPasswordValid(false);
-		} else {
-			clearErrors('confirmedPassword');
-			setConfirmedPasswordValid(true);
-		}
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>({
+		resolver: zodResolver(user),
+	});
+
+	const submitForm = (data: FormData) => {
+		const formData = {
+			fullName: `${data.firstName} ${data.lastName}`,
+			email: data.email,
+			password: data.password,
+			userType: data.userType,
+		};
+
+		console.log(formData);
+		// try {
+		// 	const response = await fetch(
+		// 		'https://localhost:7238/api/users/signup',
+		// 		{
+		// 			method: 'POST',
+		// 			headers: { 'Content-Type': 'application/json' },
+		// 			body: JSON.stringify(data),
+		// 		},
+		// 	)
+		// 		.then((res) => {
+		// 			if (res.ok) {
+		// 				alert("You're registered!");
+		// 				setServerError(null);
+		// 			} else {
+		// 				return res.json();
+		// 			}
+		// 		})
+		// 		.then((data) => {
+		// 			switch (data.code) {
+		// 				case 400:
+		// 					setServerError(
+		// 						`${data.message} [${data.code} Bad request]`,
+		// 					);
+		// 					break;
+		// 				case 401:
+		// 					setServerError(
+		// 						`${data.message} [${data.code} Unauthorized]`,
+		// 					);
+		// 					break;
+		// 				case 409:
+		// 					setServerError(
+		// 						`${data.message} [${data.code} Conflict]`,
+		// 					);
+		// 					break;
+		// 			}
+		// 		});
+		// } catch (err) {
+		// 	//TODO: check error after registration: TypeError
+		// 	//setServerError(`${err}`);
+		// }
 	};
 
 	return (
 		<form
 			className='registration-form'
 			onSubmit={handleSubmit(submitForm)}>
-			{serverError && (
-				<p className='registration-form__error-message'>
-					Registration failed due to error: {serverError}
-				</p>
-			)}
 			<div className='form-element'>
-				<label className='form-element__label'>Username:</label>
+				<label className='form-element__label'>Account:</label>
+				<select
+					className='form-element__input'
+					id='userType'
+					{...register('userType')}>
+					<option
+						className='form-element__select-option'
+						value='Client'>
+						Client
+					</option>
+					<option
+						className='form-element__select-option'
+						value='Contractor'>
+						Contractor
+					</option>
+				</select>
+				{errors.userType && (
+					<span className='form-element__validation-prompt'>
+						{errors.userType.message}
+					</span>
+				)}
+			</div>
+			<div className='form-element'>
+				<label className='form-element__label'>First name:</label>
 				<input
 					className='form-element__input'
 					type='text'
-					id='username'
-					{...register('login', {
-						required: true,
-						pattern: /^[a-zA-Z0-9]{1}[\w\d]{2,}$/,
-					})}
-					onBlur={() => {
-						if (!errors.login) {
-							setLoginValid(true);
-						}
-					}}
+					id='firstName'
+					{...register('firstName')}
 				/>
-				{errors.login && (
-					<p className='form-element__validation-prompt'>
-						Username must be at least 3 characters long.
-					</p>
+				{errors.firstName && (
+					<span className='form-element__validation-prompt'>
+						{errors.firstName.message}
+					</span>
+				)}
+			</div>
+			<div className='form-element'>
+				<label className='form-element__label'>Last name:</label>
+				<input
+					className='form-element__input'
+					type='text'
+					id='lastName'
+					{...register('lastName')}
+				/>
+				{errors.lastName && (
+					<span className='form-element__validation-prompt'>
+						{errors.lastName.message}
+					</span>
 				)}
 			</div>
 			<div className='form-element'>
@@ -126,21 +163,12 @@ export default function RegistrationForm() {
 					className='form-element__input'
 					type='email'
 					id='email'
-					{...register('email', {
-						required: true,
-						pattern:
-							/^[a-zA-Z0-9]+([.\-_]?[a-zA-Z0-9])*@[a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.[a-zA-Z]{2,}$/,
-					})}
-					onBlur={() => {
-						if (!errors.email) {
-							setEmailValid(true);
-						}
-					}}
+					{...register('email')}
 				/>
 				{errors.email && (
-					<p className='form-element__validation-prompt'>
-						Email must be a valid email address.
-					</p>
+					<span className='form-element__validation-prompt'>
+						{errors.email.message}
+					</span>
 				)}
 			</div>
 			<div className='form-element'>
@@ -149,22 +177,12 @@ export default function RegistrationForm() {
 					className='form-element__input'
 					type='password'
 					id='password'
-					{...register('password', {
-						required: true,
-						pattern: /^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).{8,}$/,
-					})}
-					onBlur={() => {
-						if (!errors.password) {
-							setPasswordValid(true);
-						}
-					}}
+					{...register('password')}
 				/>
 				{errors.password && (
-					<p className='form-element__validation-prompt'>
-						Password must be at least 8 characters long and contain
-						at least one uppercase letter, one special character,
-						and one digit.
-					</p>
+					<span className='form-element__validation-prompt'>
+						{errors.password.message}
+					</span>
 				)}
 			</div>
 			<div className='form-element'>
@@ -173,29 +191,18 @@ export default function RegistrationForm() {
 					className='form-element__input'
 					type='password'
 					id='confirmedPassword'
-					{...register('confirmedPassword', { required: true })}
-					onBlur={() => {
-						validatePasswordConfirmation();
-						if (
-							!errors.password &&
-							getValues('password') ===
-								getValues('confirmedPassword')
-						) {
-							setConfirmedPasswordValid(true);
-						}
-					}}
+					{...register('confirmedPassword')}
 				/>
 				{errors.confirmedPassword && (
-					<p className='form-element__validation-prompt'>
-						Passwords must match.
-					</p>
+					<span className='form-element__validation-prompt'>
+						{errors.confirmedPassword.message}
+					</span>
 				)}
 			</div>
 
 			<button
 				className='registration-form__submit-button'
-				type='submit'
-				disabled={!isFormValid}>
+				type='submit'>
 				Submit
 			</button>
 		</form>
