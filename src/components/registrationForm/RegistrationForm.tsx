010c7d3ecc6cd7a07a17c020c24444ae2a4c4699
marcioszka <monkiewicz.marta@gmail.com>
2023-08-19 14:08:08 +0200

    Add section under form

--- src/components/registrationForm/RegistrationForm.tsx
+++ src/components/registrationForm/RegistrationForm.tsx
@@ -46,113 +46,137 @@ export default function RegistrationForm() {
 	};
 
 	return (
-		<form
-			className='registration-form'
-			onSubmit={handleSubmit(submitForm)}>
-			{errorMessage && (
-				<span className='registration-form__error-message'>
-					{errorMessage}
-				</span>
-			)}
-			<div className='form-element'>
-				<label className='form-element__label'>Account:</label>
-				<select
-					className='form-element__input'
-					id='userType'
-					{...register('userType')}>
-					<option
-						className='form-element__select-option'
-						value='Client'>
-						Client
-					</option>
-					<option
-						className='form-element__select-option'
-						value='Contractor'>
-						Contractor
-					</option>
-				</select>
-				{errors.userType && (
-					<span className='form-element__validation-prompt'>
-						{errors.userType.message}
+		<>
+			<form
+				className='registration-form'
+				onSubmit={handleSubmit(submitForm)}>
+				{errorMessage && (
+					<span className='registration-form__error-message'>
+						{errorMessage}
 					</span>
 				)}
-			</div>
-			<div className='form-element'>
-				<label className='form-element__label'>First name:</label>
-				<input
-					className='form-element__input'
-					type='text'
-					id='firstName'
-					{...register('firstName')}
-				/>
-				{errors.firstName && (
-					<span className='form-element__validation-prompt'>
-						{errors.firstName.message}
-					</span>
-				)}
-			</div>
-			<div className='form-element'>
-				<label className='form-element__label'>Last name:</label>
-				<input
-					className='form-element__input'
-					type='text'
-					id='lastName'
-					{...register('lastName')}
-				/>
-				{errors.lastName && (
-					<span className='form-element__validation-prompt'>
-						{errors.lastName.message}
-					</span>
-				)}
-			</div>
-			<div className='form-element'>
-				<label className='form-element__label'>Email:</label>
-				<input
-					className='form-element__input'
-					type='email'
-					id='email'
-					{...register('email')}
-				/>
-				{errors.email && (
-					<span className='form-element__validation-prompt'>
-						{errors.email.message}
-					</span>
-				)}
-			</div>
-			<div className='form-element'>
-				<label className='form-element__label'>Password:</label>
-				<input
-					className='form-element__input'
-					type='password'
-					id='password'
-					{...register('password')}
-				/>
-				{errors.password && (
-					<span className='form-element__validation-prompt'>
-						{errors.password.message}
-					</span>
-				)}
-			</div>
-			<div className='form-element'>
-				<label className='form-element__label'>Confirm Password:</label>
-				<input
-					className='form-element__input'
-					type='password'
-					id='confirmedPassword'
-					{...register('confirmedPassword')}
-				/>
-				{errors.confirmedPassword && (
-					<span className='form-element__validation-prompt'>
-						{errors.confirmedPassword.message}
-					</span>
-				)}
-			</div>
+				<div className='form-element'>
+					<label className='form-element__label'>Account:</label>
+					<select
+						className='form-element__input'
+						id='userType'
+						{...register('userType')}>
+						<option
+							className='form-element__select-option'
+							value='Client'>
+							Client
+						</option>
+						<option
+							className='form-element__select-option'
+							value='Contractor'>
+							Contractor
+						</option>
+					</select>
+					{errors.userType && (
+						<span className='form-element__validation-prompt'>
+							{errors.userType.message}
+						</span>
+					)}
+				</div>
+				<div className='form-element'>
+					<label className='form-element__label'>First name:</label>
+					<input
+						className='form-element__input'
+						type='text'
+						id='firstName'
+						{...register('firstName')}
+					/>
+					{errors.firstName && (
+						<span className='form-element__validation-prompt'>
+							{errors.firstName.message}
+						</span>
+					)}
+				</div>
+				<div className='form-element'>
+					<label className='form-element__label'>Last name:</label>
+					<input
+						className='form-element__input'
+						type='text'
+						id='lastName'
+						{...register('lastName')}
+					/>
+					{errors.lastName && (
+						<span className='form-element__validation-prompt'>
+							{errors.lastName.message}
+						</span>
+					)}
+				</div>
+				<div className='form-element'>
+					<label className='form-element__label'>Email:</label>
+					<input
+						className='form-element__input'
+						type='email'
+						id='email'
+						{...register('email')}
+					/>
+					{errors.email && (
+						<span className='form-element__validation-prompt'>
+							{errors.email.message}
+						</span>
+					)}
+				</div>
+				<div className='form-element'>
+					<label className='form-element__label'>Password:</label>
+					<input
+						className='form-element__input'
+						type='password'
+						id='password'
+						{...register('password')}
+					/>
+					{errors.password && (
+						<span className='form-element__validation-prompt'>
+							{errors.password.message}
+						</span>
+					)}
+				</div>
+				<div className='form-element'>
+					<label className='form-element__label'>
+						Confirm Password:
+					</label>
+					<input
+						className='form-element__input'
+						type='password'
+						id='confirmedPassword'
+						{...register('confirmedPassword')}
+					/>
+					{errors.confirmedPassword && (
+						<span className='form-element__validation-prompt'>
+							{errors.confirmedPassword.message}
+						</span>
+					)}
+				</div>
 
-			<button
-				className='registration-form__submit-button'
-				type='submit'>
-				Submit
-			</button>
-		</form>
+				<button
+					className='registration-form__submit-button'
+					type='submit'>
+					Submit
+				</button>
+			</form>
+			<div className='divider'>
+				<div className='divider__divider-line'>
+					<hr></hr>
+				</div>
+				<div className='divider__divider-text'>OR</div>
+				<div className='divider__divider-line'>
+					<hr></hr>
+				</div>
+			</div>
+			{/* <div className='outer-registration'><button className='outer-registration__button'>Sign up with Google</button></div> */}
+			<div className='login'>
+				<p className='login__reference-text'>
+					Already have an account?{' '}
+					<a
+						href=''
+						className='login__reference-link'>
+						Log in
+					</a>
+				</p>
+			</div>
+		</>
 	);
 }
