0240ef47d7a681e29add7300762fbd97a0192942
marcioszka <monkiewicz.marta@gmail.com>
2023-08-22 01:41:13 +0200

    Add basic styles

--- src/layouts/footer/FooterLayout.tsx
+++ src/layouts/footer/FooterLayout.tsx
@@ -1,13 +1,13 @@
+import FooterComponent from '@components/footer/FooterComponent.tsx';
 import React from 'react';
-//import FooterComponent from '@components/footer/FooterComponent.tsx';
-import './FooterLayoutCss.min.css';
+import './FooterLayoutCss.css';
 
 const FooterLayout = () => {
 	return (
 		<footer
 			id='footer'
 			className='footer'>
-			{/* <FooterComponent /> */}Footer Layout
+			<FooterComponent />
 		</footer>
 	);
 };
