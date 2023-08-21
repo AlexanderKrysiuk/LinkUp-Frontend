33515a562af85f59910e142c67777a0f6721979b
marcioszka <monkiewicz.marta@gmail.com>
2023-08-22 00:53:36 +0200

    Change props

---
+++ src/components/footer/RefGroupTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RefGroupTitle = (title: string) => {
+	return (
+		<div className='footer__reference-category-title'>
+			<h4>{title}</h4>
+		</div>
+	);
+};
+
+export default RefGroupTitle;
