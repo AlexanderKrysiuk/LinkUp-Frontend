b466628a4e8a21060fdba92764ea7daedfe1d88f
marcioszka <monkiewicz.marta@gmail.com>
2023-08-22 01:04:06 +0200

    Add line component

---
+++ src/components/footer/SplitLine.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplitLine = () => {
+	return (
+		<div>
+			<hr className='footer__copyright-notice_split-line'></hr>
+		</div>
+	);
+};
+
+export default SplitLine;
