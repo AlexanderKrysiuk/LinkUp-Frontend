398b6513c9f9cf581f8430e7af3ba3392577702a
marcioszka <monkiewicz.marta@gmail.com>
2023-08-22 00:54:27 +0200

    Change interface to type

---
+++ src/components/footer/ReferenceLinks.ts
@@ -0,0 +1,46 @@
+export type Reference = {
+	title: string;
+	path: string;
+};
+
+export type ReferenceItem = {
+	groupName: string;
+	references: Reference[];
+};
+
+export const ReferenceLinks: ReferenceItem[] = [
+	{
+		groupName: 'Product',
+		references: [
+			{ title: 'How it works', path: '/Mock' },
+			{ title: 'Features', path: '/Mock' },
+			{ title: 'Pricing', path: '/Mock' },
+		],
+	},
+	{
+		groupName: 'Support',
+		references: [
+			{ title: 'FAQ', path: '/Mock' },
+			{ title: 'API documentation', path: '/Mock' },
+		],
+	},
+	{
+		groupName: 'Connect',
+		references: [
+			{ title: 'Sign up', path: '/Mock' },
+			{ title: 'Contact us', path: '/Mock' },
+		],
+	},
+];
+
+export type PolicyItem = {
+	title: string;
+	path: string;
+};
+
+export const PolicySection: PolicyItem[] = [
+	{ title: 'About us', path: '/Mock' },
+	{ title: 'Privacy', path: '/Mock' },
+	{ title: 'Terms & Conditions', path: '/Mock' },
+	{ title: 'Cookie settings', path: '/Mock' },
+];
