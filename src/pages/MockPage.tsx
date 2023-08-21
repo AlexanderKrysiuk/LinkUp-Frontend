5527b85b7bbc0bd2c606fb3be9d5164c3be35c34
marcioszka <monkiewicz.marta@gmail.com>
2023-08-21 13:34:42 +0200

    Add mock page

---
+++ src/pages/MockPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MockPageComponent = () => {
+	return (
+		<div>
+			<h1>Sample page</h1>
+			<p>Content of sample page</p>
+		</div>
+	);
+};
+
+export default MockPageComponent;
