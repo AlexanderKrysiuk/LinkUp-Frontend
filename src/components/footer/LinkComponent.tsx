90b3c758154894e1791b793ba3a83384006e5c31
marcioszka <monkiewicz.marta@gmail.com>
2023-08-21 13:33:31 +0200

    Add Link component

--- src/components/footer/LinkComponent.tsx
+++ src/components/footer/LinkComponent.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const LinkComponent = (component: string, header: string) => {
+const LinkComponent = (path: any, header: string) => {
 	return (
 		<div className='footer__link'>
-			<Link to={`/${component}`}>{header}</Link>
+			<Link to={`/${path}`}>{header}</Link>
 		</div>
 	);
 };
