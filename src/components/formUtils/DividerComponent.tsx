2388dc538e4535be30bd26ddd79da508cc26e328
marcioszka <monkiewicz.marta@gmail.com>
2023-08-23 15:48:02 +0200

    Fix variable name

--- src/components/formUtils/DividerComponent.tsx
+++ src/components/formUtils/DividerComponent.tsx
@@ -1,11 +1,12 @@
 import SplitLine from '@components/formUtils/SplitLine';
 import SplitText from '@components/formUtils/SplitText';
 import React from 'react';
-type textProp = {
+
+type TextProp = {
 	message: string;
 };
 
-const DividerComponent: React.FC<textProp> = ({ message }) => {
+const DividerComponent: React.FC<TextProp> = ({ message }) => {
 	return (
 		<div className='divider'>
 			<SplitLine />
