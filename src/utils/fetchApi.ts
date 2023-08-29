f2809106936b7d5e40b93cec4d269635826ade36
marcioszka <monkiewicz.marta@gmail.com>
2023-08-29 23:21:56 +0200

    Add credentials

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -2,7 +2,6 @@ const apiHandler = {
 	apiPost: async (url: string, payload: any) => {
 		const response = await fetch(url, {
 			method: 'POST',
-			//mode: 'no-cors',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(payload),
 			credentials: 'include',
