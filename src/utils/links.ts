78fb2c9173903ba163c46dc37d44e370b38d3fe9
marcioszka <monkiewicz.marta@gmail.com>
2023-09-01 16:40:31 +0200

    Add local uri

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,4 +1,5 @@
-export const API_URI = 'https://localhost:7190/api';
+export const LOCAL_URI = 'http://localhost:5174'
 
-export const REGISTER_URL = `${API_URI}/register`;
-export const LOGIN_URL = `${API_URI}/login`;
+export const API_URI = 'https://localhost:7190/api';
+export const API_REGISTER_URL = `${API_URI}/register`;
+export const API_LOGIN_URL = `${API_URI}/login`;
