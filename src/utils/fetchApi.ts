df73ed5a12319d8ef332fec0d0b9d2216e7b55ab
marcioszka <monkiewicz.marta@gmail.com>
2023-09-02 09:39:09 +0200

    Define POST fetch

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -8,5 +8,13 @@ const apiHandler = {
 		});
 		return response;
 	},
+	apiPost: async (url: string, payload: any) => {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(payload),
+		});
+		return response;
+	},
 };
 export default apiHandler;
