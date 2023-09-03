89f04dc705c9c3d3907e2ffbc66a974b9ba55e83
marcioszka <monkiewicz.marta@gmail.com>
2023-09-04 00:35:55 +0200

    Add url for meetings crud

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,5 +1,7 @@
-export const LOCAL_URI = 'http://localhost:5174'
+export const LOCAL_URI = 'http://localhost:5174';
 
 export const API_URI = 'https://localhost:7190/api';
 export const API_REGISTER_URL = `${API_URI}/register`;
 export const API_LOGIN_URL = `${API_URI}/login`;
+
+export const API_MEETINGS_URL = `${API_URI}/Meetings`;
