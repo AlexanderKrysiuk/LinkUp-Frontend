6798c5e4e8292287ca91b4c3651e2e4b257db541
marcioszka <monkiewicz.marta@gmail.com>
2023-09-05 15:37:27 +0200

    Refactor func to communicate with backend

---
+++ src/utils/apiHandler.ts
@@ -0,0 +1,60 @@
+import {
+	API_LOGIN_URL,
+	API_MEETINGS_URL,
+	API_REGISTER_URL,
+} from '@utils/links';
+
+export async function createUser(payload: any) {
+	return await apiOptions(API_REGISTER_URL, payload);
+}
+
+export async function loginUser(payload: any) {
+	return await apiOptions(API_LOGIN_URL, payload);
+}
+
+export async function createMeeting(payload: any, token: any) {
+	const headers = { Authorization: `Bearer ${token}` };
+	return await apiPost(API_MEETINGS_URL, payload, headers);
+}
+
+export async function getUserRole(token: any) {
+	const headers = { Authorization: `Bearer ${token}` };
+	return await apiGet(headers);
+}
+
+async function apiOptions(url: string, payload: any) {
+	const response = await fetch(url, {
+		method: 'OPTIONS',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(payload),
+		credentials: 'include',
+	});
+	return response;
+}
+
+async function apiPost(
+	url: string,
+	payload: any,
+	headers?: Record<string, string>,
+) {
+	const requestHeaders = {
+		...(headers || {}),
+		'Content-Type': 'application/json',
+	};
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: requestHeaders,
+		body: JSON.stringify(payload),
+	});
+	return response;
+}
+
+async function apiGet(headers?: Record<string, string>) {
+	const requestHeaders = { ...(headers || {}) };
+	const response = await fetch('API_USER', {
+		//add API_USER, +links.ts
+		method: 'GET',
+		headers: requestHeaders,
+	});
+	return response;
+}
