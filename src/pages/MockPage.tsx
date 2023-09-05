3a8031e24cf976b749b03316b25c242be9bead3f
marcioszka <monkiewicz.marta@gmail.com>
2023-09-05 23:27:43 +0200

    Show button if user is logged : to fix

--- src/pages/MockPage.tsx
+++ src/pages/MockPage.tsx
@@ -1,16 +1,28 @@
 //import Button from '@components/utils/Button';
 import NewMeetingForm from '@components/utils/NewMeetingForm';
 import Button from '@components/utils/buttons/Button';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MockPageComponent = () => {
 	const [isFormVisible, setIsFormVisible] = useState(false);
+
+	//const [isLogged, setIsLogged] = useState(false);
+	const [token, setToken] = useState('');
+
+	useEffect(() => {
+		const storedToken = localStorage.getItem('token');
+		if (storedToken) {
+			setToken(storedToken);
+			//setIsLogged(true);
+		}
+	}, [token]);
 
 	const toggleForm = () => {
 		setIsFormVisible(!isFormVisible);
 	};
 	return (
 		<div>
+			{token ? <h1> You are logged in, dawg!</h1> : null}
 			<Button
 				text='+ Add new meeting'
 				onClick={toggleForm}
