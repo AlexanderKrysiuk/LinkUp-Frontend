f09275984ad41fd4e53d80719f475a860ec59786
marcioszka <monkiewicz.marta@gmail.com>
2023-09-08 01:16:10 +0200

    Add meetingState to render its visibility on page

--- src/utils/mockMeetingComponent.tsx
+++ src/utils/mockMeetingComponent.tsx
@@ -14,6 +14,7 @@ const MockMeetingComponent: React.FC<MockMeetingProps> = ({
 	description,
 }: MockMeetingProps) => {
 	const [userRole, setUserRole] = useState<string>();
+	const [isDeleted, setIsDeleted] = useState<boolean>(false);
 
 	useEffect(() => {
 		const fetchUserRole = async () => {
@@ -25,6 +26,15 @@ const MockMeetingComponent: React.FC<MockMeetingProps> = ({
 
 	const date = datetime.split('T')[0];
 	const time = datetime.split('T')[1]?.slice(0, 5);
+
+	if (isDeleted) {
+		return null;
+	}
+
+	const handleDelete = (meetingId: string) => {
+		deleteMeeting(meetingId);
+		setIsDeleted(true);
+	};
 
 	return (
 		<div className='meeting'>
@@ -38,7 +48,7 @@ const MockMeetingComponent: React.FC<MockMeetingProps> = ({
 				<p className='meeting__desc'>Subject: {description}</p>
 			) : null}
 			{userRole === 'Contractor' || userRole === 'Admin' ? (
-				<button onClick={() => deleteMeeting(meetingId)}>
+				<button onClick={() => handleDelete(meetingId)}>
 					Delete meeting
 				</button>
 			) : null}
