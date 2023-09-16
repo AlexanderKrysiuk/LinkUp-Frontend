43e660a4290c211f0f90cfb312b0794e35715f1b
marcioszka <monkiewicz.marta@gmail.com>
2023-09-16 15:03:19 +0200

    Fix bugs

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,29 +1,35 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 interface AuthContextType {
 	isLogged: boolean;
 	setIsLogged: (value: boolean) => void | null;
 }
 
-export const AuthContext: React.Context<AuthContextType> = createContext<AuthContextType>({
-	isLogged: false,
-	setIsLogged: () => {},
-});
+export const AuthContext: React.Context<AuthContextType> =
+	createContext<AuthContextType>({
+		isLogged: false,
+		setIsLogged: () => {},
+	});
 
 interface AuthProviderProps {
 	children: React.ReactNode;
 }
 
-export const AuthProvider = ({children}: AuthProviderProps): JSX.Element => {
-    const [isLogged, setIsLogged] = useState(false);
-	
+export const AuthProvider = ({ children }: AuthProviderProps): JSX.Element => {
+	const [isLogged, setIsLogged] = useState(false);
+
 	useEffect(() => {
 		const token = localStorage.getItem('token');
-		if (token && !isLogged) { 
-			setIsLogged(true); 
-		} else if (!token && isLogged) { 
-			setIsLogged(false); 
-		} }, [isLogged]);
+		if (token && !isLogged) {
+			setIsLogged(true);
+		} else if (!token && isLogged) {
+			setIsLogged(false);
+		}
+	}, []);
 
-    return (<AuthContext.Provider value={{isLogged, setIsLogged}}>{children}</AuthContext.Provider>);
+	return (
+		<AuthContext.Provider value={{ isLogged, setIsLogged }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
--- src/hooks/AuthHooks.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthContext } from '@contexts/AuthContext';
-import { useContext } from 'react';
-
-type HookType = (value: boolean) => void | null;
-
-export const useSetIsLogged = (): HookType => {
-	const { setIsLogged } = useContext(AuthContext);
-	return setIsLogged;
-};
