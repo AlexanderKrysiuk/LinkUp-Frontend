895b7306aaaef500f65992d5508596af1c1764bf
marcioszka <monkiewicz.marta@gmail.com>
2023-09-18 16:37:42 +0200

    Implement set about me

---
+++ src/components/profile/header/userdetails/AboutComponent.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+
+const AboutComponent: React.FC = () => {
+	const [isEditing, setIsEditing] = useState(false);
+	const [description, setDescription] = useState('');
+
+	useEffect(() => {
+		const storedDescription = localStorage.getItem('userDescription');
+		if (storedDescription) {
+			setDescription(storedDescription);
+		} else {
+			setDescription('Tell others about yourself');
+		}
+	}, []);
+
+	const handleMouseEnter = () => {
+		if (!isEditing) {
+			setIsEditing(true);
+		}
+	};
+
+	const handleMouseLeave = () => {
+		if (isEditing) {
+			setIsEditing(false);
+		}
+	};
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDescription(event.target.value);
+	};
+
+	const handleInputBlur = () => {
+		setIsEditing(false);
+		localStorage.setItem('userDescription', description);
+	};
+
+	return (
+		<div className='profile-page'>
+			{isEditing ? (
+				<input
+					type='text'
+					value={description}
+					onChange={handleInputChange}
+					onBlur={handleInputBlur}
+				/>
+			) : (
+				<div
+					className='field'
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}>
+					{description}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default AboutComponent;
