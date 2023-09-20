efd796ee301cdd1b878fd422d1151b7d169ca143
marcioszka <monkiewicz.marta@gmail.com>
2023-09-20 15:54:00 +0200

    Update urls for upcoming and outdated meetings

--- src/data/links.ts
+++ src/data/links.ts
@@ -28,6 +28,8 @@ export const API_USER_DETAILS = `${API_URI}/user-details`;
  * The URL for meetings-related API endpoints.
  */
 export const API_MEETINGS_URL = `${API_URI}/Meetings`;
+export const API_UPCOMING_MEETINGS = `${API_MEETINGS_URL}/upcoming`;
+export const API_ARCHIVED_MEETINGS = `${API_MEETINGS_URL}/archived`;
 
 /**
  * The URL for recieving all contractors
@@ -35,4 +37,3 @@ export const API_MEETINGS_URL = `${API_URI}/Meetings`;
 export const API_CONTRACTORS = `${API_URI}/contractors`;
 
 export const API_MEETINGS_BY_USER = `${API_MEETINGS_URL}/organizator/my-meetings`;
-
