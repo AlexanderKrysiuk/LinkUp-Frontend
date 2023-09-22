5c6d5d6848071b9190ca53582dbb4960cb674c49
Alexander <alex.krysiuk@icloud.com>
2023-09-22 14:57:52 +0200

    make it be displayed

--- src/components/bookMeeting/ContractorMeetingsList.tsx
+++ src/components/bookMeeting/ContractorMeetingsList.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import Meeting from 'components/utils/Meeting';
+import React, { useEffect, useState } from 'react';
 const ContractorMeetingsList = ({ contractorEmail }: any) => {
-	return <>email:{contractorEmail}</>;
+	const [meetings, setMeetings] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			`https://localhost:5142/api/Meetings/organizator/${contractorEmail}`,
+		)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network response was not OK');
+				}
+				return response.json();
+			})
+			.then((data) => {
+				setMeetings(data);
+			})
+			.catch((error) => {
+				console.error('Error retrieving meeting data: ', error);
+			});
+	}, [contractorEmail]);
+	return (
+		<div>
+			<h2>Spotkania dla {contractorEmail}</h2>
+			<ul>
+				{meetings.map((meeting: any) => (
+					<li key={meeting.id}>
+						ID: {meeting.id}, Data i Godzina: {meeting.dateTime},
+						Opis: {meeting.description}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 export default ContractorMeetingsList;
 
