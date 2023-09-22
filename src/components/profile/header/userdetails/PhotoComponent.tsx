67da47117539cbece489d5dfaa5ca2b3a51aa7f7
marcioszka <monkiewicz.marta@gmail.com>
2023-09-22 18:12:51 +0200

    Add file input and handler

--- src/components/profile/header/userdetails/PhotoComponent.tsx
+++ src/components/profile/header/userdetails/PhotoComponent.tsx
@@ -1,35 +1,69 @@
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import { uploadPhoto } from '@middleware/userHandler';
+import 'filepond/dist/filepond.min.css';
 import React, { useState } from 'react';
-import { FilePond, registerPlugin } from 'react-filepond';
-
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+import { useNavigate } from 'react-router-dom';
 
 const PhotoComponent: React.FC = () => {
-	const [profilePhoto, setProfilePhoto] = useState<File[]>([]);
+	const [profilePhoto, setProfilePhoto] = useState(null);
+	// const [isFilePondVisible, setIsFilePondVisible] = useState(false);
+	const navigate = useNavigate();
 
-	const filePondOptions = {
-		allowMultiple: false,
-		server: '',
-		acceptedFileTypes: ['image/jpeg', 'image/jpg', 'image/bmp'],
-		maxFileSize: '1MB',
-		labelIdle:
-			'Drag & Drop your photo or <span class="filepond--label-action">Browse</span>',
+	const handlePictureChange = (event: any) => {
+		setProfilePhoto(event.target.files[0]);
 	};
 
-	const handleUpload = (files: any[]) => {
-		setProfilePhoto(files);
+	const handleUpload = async () => {
+		const form = new FormData();
+		if (profilePhoto) {
+			form.append('profilePicture', profilePhoto);
+			console.log(profilePhoto);
+		}
+		const isUploaded = await uploadPhoto(form);
+		if (isUploaded) {
+			navigate('/profile/overview', { replace: true });
+		} else {
+			navigate('/contact', { replace: true });
+		}
 	};
+
+	// const toggleVisibility = () => {
+	// 	setIsFilePondVisible(!isFilePondVisible);
+	// };
 
 	return (
-		<div className='profile__header_user-details-photo'>
-			<FilePond
-				files={profilePhoto}
-				onupdatefiles={handleUpload}
-				name='files'
-				{...filePondOptions}
-			/>
-		</div>
+		<>
+			<div className='profile__header_user-details-photo'>
+				{profilePhoto ? null : (
+					<>
+						<input
+							type='file'
+							onChange={handlePictureChange}
+						/>
+						<div onClick={handleUpload}>Upload picture</div>
+					</>
+				)}
+			</div>
+			<div>
+				<input
+					type='file'
+					onChange={handlePictureChange}
+				/>
+				<button onClick={handleUpload}>Upload picture</button>
+			</div>
+			{/* <button
+				className='profile__header_user-details-photo-button'
+				onClick={toggleVisibility}>
+				Update photo
+			</button> */}
+			{/* {isFilePondVisible ? (
+				<FilePond
+					files={profilePhoto}
+					onupdatefiles={handleUpload}
+					name='profilePicture'
+					{...filePondOptions}
+				/>
+			) : null} */}
+		</>
 	);
 };
 
