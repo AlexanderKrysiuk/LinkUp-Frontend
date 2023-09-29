73c903e94edaf1ecde992676c7dbaa8d5bf2e91f
Zachiel <ezehiel182@gmail.com>
2023-09-29 18:59:57 +0200

    feat: adding context provider and using it

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,45 +1,29 @@
-/**
- * @module HomePage
- * @description Page component for the home page.
- */
-
-import ContractorMeetingsList from '@components/bookMeeting/ContractorMeetingsList';
-import ContractorsList from '@components/bookMeeting/ContractorsList';
-import ButtonComponent from '@components/button/ButtonComponent.tsx';
-
-import { BookMeetingContext } from '@contexts/BookMeetingContext';
-import { useBackdropToggle } from '@hooks/BackdropHooks';
+import ContractorMeetingsList from '@components/bookMeeting/ContractorMeetingsList.tsx';
+import ContractorsList from '@components/bookMeeting/ContractorsList.tsx';
+import NewMeetingForm from '@components/meetingForm/NewMeetingForm.tsx';
+import { useBackdrop } from '@hooks/BackdropHooks.tsx';
+import { useModal } from '@hooks/ModalHooks.tsx';
 import React, { useState } from 'react';
 
-/**
- * Page component for the home page.
- *
- * This component represents the home page of the application. It displays a header
- * indicating the page's title and a button to toggle the backdrop. The `useBackdropToggle`
- * hook is used to manage the backdrop's visibility state.
- *
- * @component
- * @returns {JSX.Element} - The rendered home page.
- * @example
- * ```tsx
- * <HomePage />
- * ```
- */
 const HomePage = (): JSX.Element => {
-	const { toggleBackdrop } = useBackdropToggle();
+	const { toggleBackdrop } = useBackdrop();
+	const { setModal } = useModal();
 	const [contractorEmail, setContractorEmail] = useState('');
+
+	const handleToggle = () => {
+		console.log('button clicked');
+		setModal(<NewMeetingForm />);
+		toggleBackdrop();
+	};
 
 	return (
 		<>
 			<ContractorsList setContractorEmail={setContractorEmail} />
 			<ContractorMeetingsList contractorEmail={contractorEmail} />
 			<h1 className='h1'>Home</h1>
-			<button onClick={toggleBackdrop}>
-				<ButtonComponent text='Toggle Backdrop' />
-			</button>
+			<button onClick={handleToggle}>Toggle Backdrop</button>
 		</>
 	);
 };
 
 export default HomePage;
-
