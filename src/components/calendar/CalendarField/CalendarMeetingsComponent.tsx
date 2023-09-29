c97ff5ef32450c3bc25003794a0d2d0065cae276
Zachiel <ezehiel182@gmail.com>
2023-09-30 00:14:14 +0200

    feat: extracted raw meeting into readable form

--- src/components/calendar/CalendarField/CalendarMeetingsComponent.tsx
+++ src/components/calendar/CalendarField/CalendarMeetingsComponent.tsx
@@ -15,27 +15,63 @@ type CalendarMeetingsComponentProps = {
 const CalendarMeetingsComponent = ({
 	meetings,
 }: CalendarMeetingsComponentProps): JSX.Element => {
+	const calculateDuration = (meeting: Meeting) => {
+		const startTimeDate = new Date(meeting.dateTime);
+		const endTimeDate = new Date(
+			startTimeDate.setMinutes(
+				startTimeDate.getMinutes() + meeting.duration,
+			),
+		);
+		return `${new Date(meeting.dateTime).toLocaleTimeString(
+			navigator.language,
+			{
+				timeStyle: 'short',
+			},
+		)} - ${endTimeDate.toLocaleTimeString(navigator.language, {
+			timeStyle: 'short',
+		})}`;
+	};
+
 	return (
-		<>
+		<div className='calendar__meetings'>
+			{meetings[0] && (
+				<div className='calendar__meetings__header'>
+					{new Date(meetings[0].dateTime).toLocaleDateString(
+						navigator.language,
+						{
+							dateStyle: 'full',
+						},
+					)}
+				</div>
+			)}
 			{meetings.map((meeting) => {
 				return (
-					<div className='calendar__meetings'>
-						<p className='calendar__meetings_dateTime'>
-							{meeting.dateTime}
+					<div
+						key={meeting.id}
+						className='calendar__meeting'>
+						<p className='calendar__meeting-time'>
+							<span>Time:</span>
+							<span>
+								{calculateDuration(meeting)} ({meeting.duration}{' '}
+								min)
+							</span>
 						</p>
-						<p className='calendar__meetings_maxParticipants'>
-							{meeting.maxParticipants}
-						</p>
-						<p className='calendar__meetings_duration'>
-							{meeting.duration}
-						</p>
-						<p className='calendar__meetings_description'>
-							{meeting.description}
+						{meeting.description && (
+							<p className='calendar__meeting-description'>
+								<span>Description:</span>
+								<span>{meeting.description}</span>
+							</p>
+						)}
+						<p className='calendar__meeting-max-participants'>
+							<span>Participants:</span>
+							<span>
+								1 / {meeting.maxParticipants} (x slots left)
+							</span>
 						</p>
 					</div>
 				);
 			})}
-		</>
+		</div>
 	);
 };
 
