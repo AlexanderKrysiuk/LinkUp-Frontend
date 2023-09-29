3e6d58cdbe2a1b80188793c8cd30ea38cfa0af59
Zachiel <ezehiel182@gmail.com>
2023-09-29 19:30:40 +0200

    feat: temporary styling of the modal component

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -1,10 +1,11 @@
 import { useModal } from '@hooks/ModalHooks.tsx';
 import React from 'react';
+import './ModalComponent.css';
 
 const ModalComponent = (): JSX.Element | null => {
 	const { modalContent } = useModal();
 
-	return <div>{modalContent}</div>;
+	return <div className='modal'>{modalContent}</div>;
 };
 
 export default ModalComponent;
