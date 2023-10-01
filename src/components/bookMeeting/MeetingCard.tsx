bdf81349be8a0aed3ae6601f1ad5374500696345
Alexander Krysiuk <alex.krysiuk@icloud.com>
2023-10-01 20:25:53 +0200

    Working functionality but ugly code

--- src/components/bookMeeting/MeetingCard.tsx
+++ src/components/bookMeeting/MeetingCard.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './MeetingCard.css';
+
+const joinMeeting = (id: any) => {
+	const token = localStorage.getItem('token');
+	const apiUrl = 'https://localhost:5142/api/Meetings/' + id + '/join';
+
+	fetch(apiUrl, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}).then((response) => {
+		if (!response.ok) {
+			console.log(response.json());
+			throw new Error('Network response was not ok');
+		}
+	});
+};
+
 const MeetingCard = ({ meeting }: any) => {
 	return (
 		<div className='meeting-card'>
 			<h3>ID: {meeting.id}</h3>
 			<p>Data i Godzina: {meeting.dateTime}</p>
 			<p>Opis: {meeting.description}</p>
+			<button
+				className='button'
+				onClick={() => joinMeeting(meeting.id)}>
+				Join Meeting
+			</button>
 		</div>
 	);
 };
