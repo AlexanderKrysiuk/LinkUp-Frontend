bb5b33d2bed55c2f0247abbca72fce3d29d5f27c
marcioszka <monkiewicz.marta@gmail.com>
2023-10-03 00:19:38 +0200

    Docs on middleware handlers

--- src/middleware/authHandler.ts
+++ src/middleware/authHandler.ts
@@ -1,12 +1,32 @@
-import { AuthContext } from '@contexts/AuthContext';
-import { useContext, useEffect, useState } from 'react';
+/**
+ * authHandler - Module for handling authentication-related operations.
+ *
+ * This module provides functions for managing user authentication and
+ * authorization. It includes functions for setting and removing tokens from
+ * local storage, generating authentication headers, and checking user roles
+ * for authorization purposes.
+ *
+ * @module authHandler
+ */
+
 import { RoleEnum } from '@router/SecuredRoute';
 import { NavigateFunction } from 'react-router-dom';
 
+/**
+ * Generate an authentication header with the provided token.
+ *
+ * @param {string} token - The user's authentication token.
+ * @returns {object} - The authentication header.
+ */
 export const getAuthHeader = (token: string) => {
 	return { Authorization: `Bearer ${token}` };
 };
 
+/**
+ * Set the user's authentication token to local storage.
+ *
+ * @param {Response} data - The API response containing the token.
+ */
 export const setTokenToLocalStorage = async (data: Response) => {
 	try {
 		const responseData = await data.json();
@@ -21,16 +41,26 @@ export const setTokenToLocalStorage = async (data: Response) => {
 	}
 };
 
+/**
+ * Remove the user's authentication token from local storage and navigate to a specified route.
+ *
+ * @param {NavigateFunction} navigate - The function used to navigate to a route.
+ */
 export const removeTokenFromLocalStorage = (navigate: NavigateFunction) => {
 	localStorage.removeItem('token');
 	navigate('/', { replace: true });
-
 };
 
+/**
+ * Check if a user has the required role for authorization.
+ *
+ * @param {RoleEnum} user - The user's role.
+ * @param {RoleEnum} requiredRole - The required role for authorization.
+ * @returns {boolean} - True if the user has the required role; otherwise, false.
+ */
 export const checkRoleAuthorization = (
 	user: RoleEnum,
 	requiredRole: RoleEnum,
 ): boolean => {
 	return user >= requiredRole;
 };
-
