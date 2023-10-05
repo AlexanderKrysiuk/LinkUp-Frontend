2caaa3951935d6f5b677b4d25d8712c93d3c3d82
marcioszka <monkiewicz.marta@gmail.com>
2023-10-05 14:15:24 +0200

    Comment out toggle button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,31 +5,29 @@
 
 import ContractorMeetingsList from '@components/bookMeeting/ContractorMeetingsList.tsx';
 import ContractorsList from '@components/bookMeeting/ContractorsList.tsx';
-import NewMeetingForm from '@components/meetingForm/NewMeetingForm.tsx';
-import { useBackdrop } from '@hooks/BackdropHooks.tsx';
-import { useModal } from '@hooks/ModalHooks.tsx';
+// import NewMeetingForm from '@components/meetingForm/NewMeetingForm.tsx';
+// import { useBackdrop } from '@hooks/BackdropHooks.tsx';
+// import { useModal } from '@hooks/ModalHooks.tsx';
 
 import React, { useState } from 'react';
 
 const HomePage = (): JSX.Element => {
-	const { toggleBackdrop } = useBackdrop();
-	const { setModal } = useModal();
+	// const { toggleBackdrop } = useBackdrop();
+	// const { setModal } = useModal();
 
 	const [contractorEmail, setContractorEmail] = useState('');
 
-	const handleToggle = () => {
-		setModal(<NewMeetingForm />);
-		toggleBackdrop();
-	};
+	// const handleToggle = () => {
+	// 	setModal(<NewMeetingForm />);
+	// 	toggleBackdrop();
+	// };
 
 	return (
 		<>
 			<ContractorsList setContractorEmail={setContractorEmail} />
 			<ContractorMeetingsList contractorEmail={contractorEmail} />
-			{/* <h1 className='h1'>Home</h1>
-			<button onClick={handleToggle}>
-				<ButtonComponent text='Toggle Backdrop' />
-			</button> */}
+			<h1 className='h1'>Home</h1>
+			{/* <button onClick={handleToggle}>Toggle Backdrop</button> */}
 		</>
 	);
 };
